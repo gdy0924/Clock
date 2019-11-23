2324ca9830e67ba5d47c5655a92c5696f5d04aed
gdy0924 <44692673+gdy0924@users.noreply.github.com>
2019-11-24 00:42:28 +0800

    Clock

--- app/src/main/java/com/example/clock/AlarmFragment.java
+++ app/src/main/java/com/example/clock/AlarmFragment.java
@@ -1,15 +1,33 @@
 package com.example.clock;
 
-
+import androidx.appcompat.widget.Toolbar;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
+
+import org.litepal.LitePal;
+import org.litepal.crud.DataSupport;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,10 +35,43 @@ import android.widget.TextView;
 public class AlarmFragment extends Fragment {
 
 
+    public static List<Clock> list = new ArrayList<>();
+    public static TimeAdapter timeAdapter;
+    RecyclerView recyclerView;
+//    NavigationView navigationView;
+//    DrawerLayout drawerLayout;
+//    private ImageView more;
+    TextView title;
+    Context context = getActivity();
+
     public AlarmFragment() {
         // Required empty public constructor
     }
 
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    private void initTitle() {
+        title.setText("你的闹钟");
+    }
+
+    private void initRecyclerView() {
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        timeAdapter = new TimeAdapter(list, context);
+        recyclerView.setAdapter(timeAdapter);
+        list.clear();
+        List<Clock> list1 = DataSupport.findAll(Clock.class);
+        for (Clock clock : list1) {
+            list.add(clock);
+        }
+        timeAdapter.notifyDataSetChanged();
+    }
 
     @Nullable
     @Override
@@ -32,13 +83,11 @@ public class AlarmFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-//        textView=(TextView)getActivity().findViewById(R.id.textView1);
-//        button=(Button)getActivity().findViewById(R.id.button1);
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override            public void onClick(View view) {
-//                Toast.makeText(getActivity(),"Fragment1",Toast.LENGTH_SHORT).show();
-//            }
-//        });
+        title = getActivity().findViewById(R.id.title);
+        recyclerView = getActivity().findViewById(R.id.clock_list);
+        initTitle();
+        LitePal.getDatabase();
+        initRecyclerView();
     }
 
 }
